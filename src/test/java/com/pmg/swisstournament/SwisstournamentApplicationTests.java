639c1a0d1eb977446827be8a216e430822bac8f1
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2023-08-31 19:26:56 -0700

    Init project

---
+++ src/test/java/com/pmg/swisstournament/SwisstournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmg.swisstournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwisstournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
